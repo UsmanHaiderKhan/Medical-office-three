9ca7a88ca3ed357b8ad3079dad3d4c6b11feae77
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-26 23:12:00 +0500

    Update the bootstrap 4 environment

---
+++ Medical-office-three/assets/Bootstrap4/js/script.js
@@ -0,0 +1,137 @@
+﻿//console.log("usman");
+
+/*================== Read More Text ==================*/
+
+$(function () {
+    var showChar = 145;
+    var moretext = "Read More";
+    var lesstext = "Read Less";
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-contents"><span>' + hide_content + '</span>' +
+                '<a href="" class="morelinksss read-text" style="display:block;margin-top:30px;position: absolute;">' + moretext + '</a>'
+                + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinksss").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+/*===================== Smooth Scrolling ======================*/
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Nav Active Class =======================*/
+
+$(function () {
+    $('.nav-link').on('click',
+        function () {
+            $('.nav-link').removeClass('active');
+            $(this).addClass('active');
+        });
+});
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 4,
+        showItems: 4
+
+    });
+
+});
+
+/*===================== Date Picker Function ======================*/
+$(function () {
+    $("#datepicker").datepicker();
+});
+/*===================== Owl Carousel Slider ======================*/
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 30,
+        dots: false,
+        nav: true,
+        responsiveClass: true,
+        navText: ["<i class='flaticon-left'></i>", "<i class='flaticon-next'></i>"],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            400: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            600: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            800: {
+                items: 1,
+                dots: false,
+                nav: true
+            },
+            1000: {
+                items: 3,
+                dots: false,
+                nav: true
+            }
+        }
+    });
+});
+/*===================== Slick Slider ======================*/
+var counters = [];
+counters = $(".counter-value");
+var countersQuantity = counters.length;
+console.log(counters + "," + countersQuantity);
+var counter = [];
+
+for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+}
+var count = function (start, value, id) {
+    var localStart = start;
+    setInterval(function () {
+        if (localStart < value) {
+            localStart++;
+            counters[id].innerHTML = localStart;
+        }
+    }, 1);
+}
+var cond = true;
+requestScrollAnimation((pos) => {
+    var elAlert = document.getElementById("counter-strike");
+    var isVisible = isScrolledIntoView(elAlert, true);
+
+    if (isVisible) {
+        if (cond) {
+            for (var j = 0; j < countersQuantity; j++) {
+                count(0, counter[j], j);
+            }
+            cond = false;
+        }
+
+
+    }
+});
